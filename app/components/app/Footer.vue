18d9e0e2098c07b3e73a50f33d66481f94d689b0
SE-Designs <dragonlorefnstattrack@gmail.com>
2025-08-15 15:41:02 +0500

    upd: footer

--- app/components/app/Footer.vue
+++ app/components/app/Footer.vue
@@ -1,26 +1,9 @@
 <script setup lang="ts">
-
+import RELEASE from '~/const/release';
 </script>
 <template>
   <footer>
-    <div class="flex items-center h-full justify-between max-width mx-auto px-8">
-      <div class="flex flex-col">
-        <NuxtLink to="/" class="text-inherit">
-          <h4 class="text-base">Frontend Legends</h4>
-        </NuxtLink>
-        <p class="font-main op-60">Advanced frontend guidelines</p>
-      </div>
-    </div>
+    <span>{{ RELEASE }}</span>
   </footer>
 </template>
-<style scoped>
-footer {
-  width: 100%;
-
-  padding: 24px;
-  color: var(--on-light);
-  background-color: var(--on-dark);
-
-  border-top: 1px solid var(--gray);
-}
-</style>
+<style scoped></style>
