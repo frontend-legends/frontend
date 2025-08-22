5c29eea5713be7eb0ffb08fefd67fae9b28d22b2
SE-Designs <dragonlorefnstattrack@gmail.com>
2025-08-22 09:59:14 +0500

    upd: nav, page progress

--- app/composables/usePageProgress.ts
+++ app/composables/usePageProgress.ts
@@ -1,29 +1,34 @@
 export default function () {
-    const scrollY = ref(0);
-  
-    const { height: windowHeight } = useWindowSize();
-  
-    const updateScroll = () => {
-      scrollY.value = window.scrollY;
-    };
-  
-    const debouncedUpdateScroll = useDebounceFn(updateScroll, 5);
-  
-    const scrollProgress = computed(() => {
-      const documentHeight = document.documentElement.scrollHeight;
-      const scrollableHeight = documentHeight - windowHeight.value;
-      return Number((scrollY.value / scrollableHeight) * 100);
-    });
-  
-    onMounted(() => {
-      window.addEventListener("scroll", debouncedUpdateScroll);
-      debouncedUpdateScroll();
-    });
-  
-    onUnmounted(() => {
-      window.removeEventListener("scroll", debouncedUpdateScroll);
-    });
-  
-    return { scrollProgress };
-  }
-  
+  const scrollY = ref(0);
+
+  const { height: windowHeight } = useWindowSize();
+
+  const updateScroll = () => {
+    scrollY.value = window.scrollY;
+  };
+
+  const debouncedUpdateScroll = useDebounceFn(updateScroll, 5);
+
+  const scrollProgress = computed(() => {
+    const documentHeight = document.documentElement.scrollHeight;
+    const scrollableHeight = documentHeight - windowHeight.value;
+
+    const value =
+      Number((scrollY.value / scrollableHeight) * 100) > 100
+        ? 100
+        : Number((scrollY.value / scrollableHeight) * 100);
+
+    return value;
+  });
+
+  onMounted(() => {
+    window.addEventListener("scroll", debouncedUpdateScroll);
+    debouncedUpdateScroll();
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener("scroll", debouncedUpdateScroll);
+  });
+
+  return { scrollProgress };
+}
